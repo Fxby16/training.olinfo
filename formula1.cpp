a38131966f34f710dc54044c971e2accccbf0531
Faby <105114784+Fxby16@users.noreply.github.com>
2023-05-28 17:31:08 +0200

    Create formula1.cpp

---
+++ formula1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#pragma GCC optmize ("O3")
+using namespace std;
+#define ll long long
+#define pll pair<ll,ll>
+#define MAXN 2000
+
+int A[MAXN],B[MAXN],C[MAXN];
+
+int main(){
+ios::sync_with_stdio(false);
+cin.tie(0);
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+int n;
+    cin>>n;
+vector<pll>v(n);
+    for(int i=0;i<n;i++)
+        cin>>A[i]>>B[i]>>C[i];
+int q;
+    cin>>q;
+    while(q--){
+        ll k,t;
+        cin>>k>>t;
+        for(int i=0;i<n;i++)
+            v[i]={-(A[i]*t*t+B[i]*t+C[i]),i+1};
+        nth_element(v.begin(),v.begin()+k-1,v.end());
+        cout<<v[k-1].second<<'\n';
+    }
+
+    return 0;
+}
